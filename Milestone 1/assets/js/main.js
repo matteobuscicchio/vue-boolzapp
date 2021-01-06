ccaac950ec79224c2f243c060aa738829a9e5e10
matteo <matteoezio@me.com>
2021-01-06 03:04:17 +0100

    milestone 1 completato

--- Milestone 1/assets/js/main.js
+++ Milestone 1/assets/js/main.js
@@ -1,85 +1,98 @@
-contacts: [
-    {
-        name: 'Michele',
-        avatar: '_1',
-        visible: true,
-        messages: [
-            {
+let boolzApp = new Vue({
+    el: '#app_container',
+    data: {
+        contactIndex: 0,
+        contacts: [{
+            name: 'Michele',
+            avatar: '_1',
+            visible: true,
+            messages: [{
                 date: '10/01/2020 15:30:55',
                 text: 'Hai portato a spasso il cane?',
-                status: 'sent'
+                status: 'sent',
+                deleteShow: false
             },
             {
                 date: '10/01/2020 15:50:00',
                 text: 'Ricordati di dargli da mangiare',
-                status: 'sent'
+                status: 'sent',
+                deleteShow: false
             },
             {
                 date: '10/01/2020 16:15:22',
                 text: 'Tutto fatto!',
-                status: 'received'
-            }
-        ],
-    },
-    {
-        name: 'Fabio',
-        avatar: '_2',
-        visible: true,
-        messages: [
-            {
+                status: 'received',
+                deleteShow: false
+            }],
+        },
+        {
+            name: 'Fabio',
+            avatar: '_2',
+            visible: true,
+            messages: [{
                 date: '20/03/2020 16:30:00',
                 text: 'Ciao come stai?',
-                status: 'sent'
+                status: 'sent',
+                deleteShow: false
             },
             {
                 date: '20/03/2020 16:30:55',
                 text: 'Bene grazie! Stasera ci vediamo?',
-                status: 'received'
+                status: 'received',
+                deleteShow: false
             },
             {
                 date: '20/03/2020 16:35:00',
                 text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                status: 'sent'
-            }
-        ],
-    },
-    {
-        name: 'Samuele',
-        avatar: '_3',
-        visible: true,
-        messages: [
-            {
+                status: 'sent',
+                deleteShow: false
+            }],
+        },
+        {
+            name: 'Samuele',
+            avatar: '_3',
+            visible: true,
+            messages: [{
                 date: '28/03/2020 10:10:40',
                 text: 'La Marianna va in campagna',
-                status: 'received'
+                status: 'received',
+                deleteShow: false
             },
             {
                 date: '28/03/2020 10:20:10',
                 text: 'Sicuro di non aver sbagliato chat?',
-                status: 'sent'
+                status: 'sent',
+                deleteShow: false
             },
             {
                 date: '28/03/2020 16:15:22',
                 text: 'Ah scusa!',
-                status: 'received'
-            }
-        ],
-    },
-    {
-        name: 'Luisa',
-        avatar: '_4',
-        visible: true,
-        messages: [
-            {
+                status: 'received',
+                deleteShow: false
+            }],
+        },
+        {
+            name: 'Luisa',
+            avatar: '_4',
+            visible: true,
+            messages: [{
                 date: '10/01/2020 15:30:55',
                 text: 'Lo sai che ha aperto una nuova pizzeria?',
-                status: 'sent'
+                status: 'sent',
+                deleteShow: false
             },
             {
                 date: '10/01/2020 15:50:00',
                 text: 'Si, ma preferirei andare al cinema',
-                status: 'received'
-            }
-        ],
+                status: 'received',
+                deleteShow: false
+            }],
+        },]
+        
     },
-]
+    methods: {
+        corrispondentContact: function(index){
+            this.contactIndex = index;
+        },
+    },
+})
